4bed1267b264a37351013c7bf312cc892b9983a7
s1j1k <89644676+s1j1k@users.noreply.github.com>
2024-05-31 02:54:04 +1000

    try to dix data error

--- src/data_utils.py
+++ src/data_utils.py
@@ -217,7 +217,9 @@ class TextDataset(Dataset):
                 if param.device != device:
                     param.data = param.data.to(device)
 
-            hidden_state = self.model_emb(torch.tensor(input_ids))
+            input_ids_tensor = torch.tensor(input_ids).to(device)
+
+            hidden_state = self.model_emb(input_ids_tensor)
 
             arr = np.array(hidden_state, dtype=np.float32)
 
